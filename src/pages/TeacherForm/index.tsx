3cb8f8b5c805330f31ac38e3d2917a5af665d5a6
Greyson Mascarenhas <greysonmrx@gmail.com>
2020-08-03 12:00:50 -0300

    feat: create teacher form page

---
+++ src/pages/TeacherForm/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Header from '../../components/Header';
+
+function TeacherForm() {
+  return (
+    <div id="page-teacher-list" className="container">
+      <Header title="Que incrível que você quer dar aulas." />
+    </div>
+  );
+}
+
+export default TeacherForm;
