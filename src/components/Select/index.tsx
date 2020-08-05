2e216f250eefe5ddd3112fc09f0b4ab101210f0b
Greyson Mascarenhas <greysonmrx@gmail.com>
2020-08-05 15:19:10 -0300

    feat: create select component

---
+++ src/components/Select/index.tsx
@@ -0,0 +1,29 @@
+import React, { SelectHTMLAttributes } from 'react';
+
+import './styles.css';
+
+interface SelectProps extends SelectHTMLAttributes<HTMLSelectElement> {
+  label: string;
+  name: string;
+  options: Array<{
+    value: string;
+    label: string;
+  }>;
+}
+
+const Select: React.FC<SelectProps> = ({ label, name, options, ...props }) => {
+  return (
+    <div className="select-block">
+      <label htmlFor={name}>{label}</label>
+      <select defaultValue="" {...props} id={name}>
+        <option value="" disabled hidden>Selecione uma opção</option>
+
+        {options.map(option => (
+          <option key={option.value} value={option.value}>{option.label}</option>
+        ))}
+      </select>
+    </div>
+  );
+}
+
+export default Select;
